3a77d4c7c06918ddd766740a4ce475f87add6160
chetkiy777 <egunka1988@gmail.com>
2025-01-10 10:54:14 +0200

    start2

--- app/page.js
+++ app/page.js
@@ -1,25 +1,27 @@
-'use client'
+// 'use client'
 import styles from "./page.module.css";
-import {useEffect, useState} from "react";
+// import {useEffect, useState} from "react";
+import LocationPage from "@/app/location/page";
 
 const Home = () => {
 
-    const [swData, setSwData] = useState(null);
+    // const [swData, setSwData] = useState(null);
+    //
+    // useEffect(() => {
+    //     fetch('https://swapi.py4e.com/api/people/1/')
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setSwData(data.name)
+    //         })
+    // }, []);
 
-    useEffect(() => {
-        fetch('https://swapi.py4e.com/api/people/1/')
-            .then((res) => res.json())
-            .then((data) => {
-                setSwData(data.name)
-            })
-    }, []);
 
-
-    if (!swData) return <div>Loading...</div>
+    // if (!swData) return <div>Loading...</div>
 
   return (
     <div className={styles.page}>
-        {swData}
+        <LocationPage />
+        {/*{swData}*/}
     </div>
   );
 }
