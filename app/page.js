f343b3007730f9a48129ed89f162616fc0c732fe
chetkiy777 <egunka1988@gmail.com>
2025-01-10 11:07:31 +0200

    start2

--- app/page.js
+++ app/page.js
@@ -1,22 +1,23 @@
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import GoodMarkup from "@/app/components/GoodMarkup";
+import BadMarkup from "@/app/components/BadMarkup";
+
 
 export const dynamic = 'force-dynamic'; // Указывает, что страница всегда должна генерироваться сервером
 
 export default async function HomePage() {
 
-    const token = process.env.API_TOKEN;
+    // const token = process.env.API_TOKEN;
 
     // Получение данных геолокации
-    const geoResponse = await fetch(`https://ipinfo.io/json?token=${token}`, {
+    const geoResponse = await fetch(`https://ipinfo.io/json?token=20a86765720c74`, {
         cache: 'no-store', // Отключаем кеширование для получения актуальных данных
     });
     const geoData = await geoResponse.json();
+    const country = geoData.country;
 
     return (
         <main>
-            <h1>Добро пожаловать!</h1>
-            <p>Ваш город: {geoData.city || 'Неизвестно'}</p>
-            <p>Ваша страна: {geoData.country || 'Неизвестно'}</p>
+            {country && country === "PL" ? <GoodMarkup /> : <BadMarkup /> }
         </main>
     );
 }
