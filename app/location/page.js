edb66a478af16cb25d2d0cf2e77fcb5789e5c751
chetkiy777 <egunka1988@gmail.com>
2025-01-10 10:42:16 +0200

    start

--- app/location/page.js
+++ app/location/page.js
@@ -2,7 +2,6 @@ export const dynamic = 'force-dynamic';
 
 export default async function LocationPage({ geo }) {
     const location = geo || { city: 'Неизвестно', country: 'Неизвестно' };
-
     return (
         <div>
             <p>Ваш город: {location.city}</p>
