50ee82b0b0448e233bb71ec44cf733cee8fb9c9b
chetkiy777 <egunka1988@gmail.com>
2025-01-10 11:41:52 +0200

    start2

--- app/page.js
+++ app/page.js
@@ -1,22 +1,33 @@
+'use client'
+import { useEffect, useState } from 'react';
 import GoodMarkup from "@/app/components/GoodMarkup";
 import BadMarkup from "@/app/components/BadMarkup";
 
-export const dynamic = 'force-dynamic'; // Указывает, что страница всегда должна генерироваться сервером
+export default function Home() {
+    const [geoData, setGeoData] = useState('');
 
-export default async function HomePage() {
+    useEffect(() => {
+        async function fetchIP() {
+            const res = await fetch('https://api.ipify.org?format=json'); // API для получения IP
+            const data = await res.json();
 
+            const geoResponse = await fetch(`https://ipinfo.io/${data.ip}/json?token=20a86765720c74`, {
+                cache: 'no-store',
+            });
+            const geoData = await geoResponse.json();
+            console.log(geoData);
+            setGeoData(geoData.country)
 
-    // Запрос к сервису геолокации
-    const geoResponse = await fetch(`https://ipinfo.io/?token=20a86765720c74`);
+        }
 
-    // Получение данных геолокации
-    const geoData = await geoResponse.json();
-    const country = geoData.country;
+        fetchIP();
+    }, []);
+
 
     return (
         <main>
-            {country && country === "UA" ? <GoodMarkup /> : <BadMarkup /> }
-            <div>{country}</div>
+            {geoData && geoData === "UA" ? <GoodMarkup /> : <BadMarkup />}
+            <p>{geoData}</p>
         </main>
     );
 }
