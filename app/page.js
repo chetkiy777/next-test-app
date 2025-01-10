1803bb8ee27f29e1128da6bb33001dd59ddb3b2f
chetkiy777 <egunka1988@gmail.com>
2025-01-10 11:23:26 +0200

    start2

--- app/page.js
+++ app/page.js
@@ -4,14 +4,17 @@ import BadMarkup from "@/app/components/BadMarkup";
 
 export const dynamic = 'force-dynamic'; // Указывает, что страница всегда должна генерироваться сервером
 
-export default async function HomePage() {
+export default async function HomePage(context) {
 
-    // const token = process.env.API_TOKEN;
 
-    // Получение данных геолокации
-    const geoResponse = await fetch(`https://ipinfo.io/json?token=20a86765720c74`, {
+    const clientIP = context.req.headers['x-forwarded-for'] || context.req.connection.remoteAddress;
+
+    // Запрос к сервису геолокации
+    const geoResponse = await fetch(`https://ipinfo.io/${clientIP}?token=20a86765720c74`, {
         cache: 'no-store', // Отключаем кеширование для получения актуальных данных
     });
+
+    // Получение данных геолокации
     const geoData = await geoResponse.json();
     const country = geoData.country;
 
